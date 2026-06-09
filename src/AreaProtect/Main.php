b234b43303c99e202b0c21c314b7461687543164
ZyberPE <zyberpe@gmail.com>
2026-06-09 03:07:03 -0400

    Create Main.php

---
+++ src/AreaProtect/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AreaProtect;
+
+use pocketmine\plugin\PluginBase;
+use AreaProtect\area\AreaManager;
+
+final class Main extends PluginBase{
+
+    private static Main $instance;
+    private AreaManager $areaManager;
+
+    public static function getInstance() : self{
+        return self::$instance;
+    }
+
+    protected function onEnable() : void{
+        self::$instance = $this;
+
+        @mkdir($this->getDataFolder());
+
+        $this->saveResource("areas.yml");
+
+        $this->areaManager = new AreaManager($this);
+
+        $this->getServer()->getPluginManager()->registerEvents(
+            new \AreaProtect\listener\EventListener(),
+            $this
+        );
+    }
+
+    public function getAreaManager() : AreaManager{
+        return $this->areaManager;
+    }
+}
